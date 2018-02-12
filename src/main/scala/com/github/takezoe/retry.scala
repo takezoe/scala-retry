19bdfa04cb5986fb1b59a1e8e191519ae11f7843
Naoki Takezoe <takezoe@gmail.com>
2018-02-12 14:14:18 +0900

    Future support

--- src/main/scala/com/github/takezoe/retry.scala
+++ src/main/scala/com/github/takezoe/retry.scala
@@ -4,7 +4,8 @@ import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicBoolean
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
 
 object retry {
 
@@ -30,6 +31,10 @@ object retry {
     retryManager.schedule(f)
   }
 
+  def retryFuture[T](f: => Future[T])(implicit config: RetryConfig, retryManager: RetryManager, ec: ExecutionContext): Future[T] = {
+    retryManager.scheduleFuture(f)
+  }
+
   case class RetryConfig(maxAttempts: Int, retryDuration: Duration, backOff: Boolean)
 
   class RetryManager {
@@ -45,18 +50,36 @@ object retry {
           tasks.iterator().forEachRemaining { task =>
             if(task.nextRun <= currentTime){
               tasks.remove(task)
-              try {
-                val result = task.f()
-                task.promise.success(result)
-              } catch {
-                case e: Exception =>
-                  if(task.count == task.config.maxAttempts){
-                    task.promise.failure(e)
-                  } else {
-                    val count = task.count + 1
-                    val nextRun = currentTime + (if(task.config.backOff) task.config.retryDuration.toMillis * count else task.config.retryDuration.toMillis)
-                    tasks.add(new RetryTask(task.f, task.config, task.promise, nextRun, count))
+
+              task match {
+                case task: BlockingRetryTask =>
+                  try {
+                    val result = task.f()
+                    task.promise.success(result)
+                  } catch {
+                    case e: Exception =>
+                      if(task.count == task.config.maxAttempts){
+                        task.promise.failure(e)
+                      } else {
+                        val count = task.count + 1
+                        val nextRun = currentTime + (if(task.config.backOff) task.config.retryDuration.toMillis * count else task.config.retryDuration.toMillis)
+                        tasks.add(new BlockingRetryTask(task.f, task.config, task.promise, nextRun, count))
+                      }
                   }
+                case task: FutureRetryTask =>
+                  val future = task.f()
+                  future.onComplete {
+                    case Success(v) => task.promise.success(v)
+                    case Failure(e) => {
+                      if(task.count == task.config.maxAttempts){
+                        task.promise.failure(e)
+                      } else {
+                        val count = task.count + 1
+                        val nextRun = currentTime + (if(task.config.backOff) task.config.retryDuration.toMillis * count else task.config.retryDuration.toMillis)
+                        tasks.add(new FutureRetryTask(task.f, task.config, task.ec, task.promise, nextRun, count))
+                      }
+                    }
+                  }(task.ec)
               }
             }
           }
@@ -69,7 +92,14 @@ object retry {
 
     def schedule[T](f: => T)(implicit config: RetryConfig): Future[T] = {
       val promise = Promise[T]()
-      val task = new RetryTask(() => f, config, promise.asInstanceOf[Promise[Any]], System.currentTimeMillis + config.retryDuration.toMillis)
+      val task = new BlockingRetryTask(() => f, config, promise.asInstanceOf[Promise[Any]], System.currentTimeMillis + config.retryDuration.toMillis)
+      tasks.add(task)
+      promise.future
+    }
+
+    def scheduleFuture[T](f: => Future[T])(implicit config: RetryConfig, ec: ExecutionContext): Future[T] = {
+      val promise = Promise[T]()
+      val task = new FutureRetryTask(() => f, config, ec, promise.asInstanceOf[Promise[Any]], System.currentTimeMillis + config.retryDuration.toMillis)
       tasks.add(task)
       promise.future
     }
@@ -80,12 +110,25 @@ object retry {
 
   }
 
-  private[retry] class RetryTask(
+  private[retry] sealed trait RetryTask {
+    val nextRun: Long
+  }
+
+  private[retry] class BlockingRetryTask(
     val f: () => Any,
     val config: RetryConfig,
     val promise: Promise[Any],
     val nextRun: Long,
     val count: Int = 0
-  )
+  ) extends RetryTask
+
+  private[retry] class FutureRetryTask(
+    val f: () => Future[Any],
+    val config: RetryConfig,
+    val ec: ExecutionContext,
+    val promise: Promise[Any],
+    val nextRun: Long,
+    val count: Int = 0
+  ) extends RetryTask
 
 }
