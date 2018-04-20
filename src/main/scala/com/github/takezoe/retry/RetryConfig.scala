b22b48d8ed208c0d7245c0a2fe90db6d8c658382
Naoki Takezoe <takezoe@gmail.com>
2018-04-20 19:05:54 +0900

    Provide some types of default RetryConfig

--- src/main/scala/com/github/takezoe/retry/RetryConfig.scala
+++ src/main/scala/com/github/takezoe/retry/RetryConfig.scala
@@ -1,9 +1,17 @@
 package com.github.takezoe.retry
 
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.{Duration, FiniteDuration}
 
 case class RetryConfig(
   maxAttempts: Int,
   retryDuration: FiniteDuration,
   backOff: BackOff
 )
+
+object RetryConfig {
+
+  val NoRetry = RetryConfig(0, Duration.Zero, FixedBackOff)
+
+  def Immediately(attempts: Int): RetryConfig = RetryConfig(attempts, Duration.Zero, FixedBackOff)
+
+}
